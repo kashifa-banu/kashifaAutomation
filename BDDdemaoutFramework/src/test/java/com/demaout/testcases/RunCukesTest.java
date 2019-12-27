c45fa866855b23d648a2c7beb4fc2197e1d24550
KashifaBanu <KashifaBanu@9.197.254.125>
2019-12-27 14:13:22 +0530

    abc

--- BDDdemaoutFramework/src/test/java/com/demaout/testcases/RunCukesTest.java
+++ BDDdemaoutFramework/src/test/java/com/demaout/testcases/RunCukesTest.java
@@ -7,7 +7,7 @@ import cucumber.junit.Cucumber;
 @RunWith(Cucumber.class)
 @Cucumber.Options(format = {"pretty", "html:target/cucumber-html-report",
 		"json-pretty:target/cucumber-report.json"},
-features = {"classpath:BDDdemaoutFramework/src/resources/com/demaout/testcases/demaout.feature"})
+features = {"classpath:src/resources/com/demaout/testcases/demaout.feature"})
 
 public class RunCukesTest {
 
